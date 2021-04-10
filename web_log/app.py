29c26b95ab5a3e2c9ee47cc07edae49cb48bbd4e
jesussantana <jesus@esmarketingdigital.com>
2021-04-10 15:58:53 +0200

    Update Graphs & Dash App

--- web_log/app.py
+++ web_log/app.py
@@ -6,9 +6,9 @@ import pandas as pd
 # load data
 data = pd.read_csv("data/Logs_export.csv")
 
-data = data.query("DNS == 'www.akumenius.com' and COUNTRY_NAME == 'Spain'")
-# data["Date"] = pd.to_datetime(data["DATE"], format="%Y-%m-%d")
-data.sort_values("DATE", inplace=True)
+data1 = data.groupby('DNS')[['DATE']].count().sort_values(by='DATE',ascending=False).reset_index()
+data2 = data.groupby('COUNTRY_NAME')[['DNS']].count().sort_values(by='DNS',ascending=False).reset_index().head(5)
+data3 = data.groupby('CITY')[['DNS']].count().sort_values(by='DNS',ascending=False).reset_index().head(5)
 
 app = dash.Dash(__name__)
 
@@ -16,31 +16,44 @@ app.layout = html.Div(
     children=[
         html.H1(children="Web Logs Analytics",),
         html.P(
-            children="Analyze the behavior of visits"
+            children="Analyze the behavior of vviews"
             " to akumenius.com by date",
         ),
         dcc.Graph(
             figure={
                 "data": [
                     {
-                        "x": data.DATE,
-                        "y": data.DNS.count(),
+                        "x": data1.DNS,
+                        "y": data1.DATE,
                         "type": "bar",
                     },
                 ],
-                "layout": {"title": "Web Visits"},
+                "layout": {"title": "Web Views"},
             },
         ),
         dcc.Graph(
             figure={
                 "data": [
                     {
-                        "x": data.DNS,
-                        "y": data.CITY,
+                        "x": data2.COUNTRY_NAME,
+                        "y": data2.DNS,
+                        "type": "bar",
+                        "oriented": "h"
+                    },
+                ],
+                "layout": {"title": "Country Views"},
+            },
+        ),
+        dcc.Graph(
+            figure={
+                "data": [
+                    {
+                        "x": data3.CITY,
+                        "y": data3.DNS,
                         "type": "bar",
                     },
                 ],
-                "layout": {"title": "City Visits"},
+                "layout": {"title": "City Views"},
             },
         ),
     ]
