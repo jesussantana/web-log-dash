021e36d60cee5c814819f670c1fe1f63e9476095
jesussantana <jesus@esmarketingdigital.com>
2021-04-10 20:05:44 +0200

    Testing Plotly

--- web_log/app.py
+++ web_log/app.py
@@ -1,23 +1,40 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+from dash.dependencies import Input, Output
+import plotly.express as px
+import plotly.io as pio
 import pandas as pd
 
+template = "plotly_dark"
 # load data
 data = pd.read_csv("data/Logs_export.csv")
 
 data1 = data.groupby('DNS')[['DATE']].count().sort_values(by='DATE',ascending=False).reset_index()
 data2 = data.groupby('COUNTRY_NAME')[['DNS']].count().sort_values(by='DNS',ascending=False).reset_index().head(5)
-data3 = data.groupby('CITY')[['DNS']].count().sort_values(by='DNS',ascending=False).reset_index().head(5)
+data3 = data.groupby('CITY')[['DNS']].count().sort_values(by='DNS',ascending=False).reset_index().head(25)
 
 app = dash.Dash(__name__)
+
+'''import plotly.graph_objects as go
+
+df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/2014_apple_stock.csv')
+
+fig = go.Figure(go.Scatter(x = df['AAPL_x'], y = df['AAPL_y'],
+                  name='Share Prices (in USD)'))
+
+fig.update_layout(title='Apple Share Prices over time (2014)',
+                   plot_bgcolor='rgb(230, 230,230)',
+                   showlegend=True)
+
+fig.show()'''
 
 app.layout = html.Div(
     children=[
         html.H1(children="Web Logs Analytics",),
         html.P(
             children="Analyze the behavior of vviews"
-            " to akumenius.com by date",
+            " to akumenius.com",
         ),
         dcc.Graph(
             figure={
@@ -25,20 +42,22 @@ app.layout = html.Div(
                     {
                         "x": data1.DNS,
                         "y": data1.DATE,
-                        "type": "bar",
+                        "type": "scatter",
                     },
                 ],
-                "layout": {"title": "Web Views"},
+                
+                "layout": {"title": "Web Views", "template":"template"},
             },
         ),
         dcc.Graph(
             figure={
                 "data": [
                     {
-                        "x": data2.COUNTRY_NAME,
-                        "y": data2.DNS,
+                        "x": data2.DNS,
+                        "y": data2.COUNTRY_NAME,
                         "type": "bar",
-                        "oriented": "h"
+                        'orientation':'h',
+                        'color':'COUNTRY_NAME',
                     },
                 ],
                 "layout": {"title": "Country Views"},
